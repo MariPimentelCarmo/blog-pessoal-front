9460a7241234ffd14fa981d42b60f41fdab70a41
MariPimentelCarmo <mariana.carmo@sptech.school>
2025-04-15 20:17:17 -0300

    Postagem implementado

--- blogpessoal/src/components/navbar/Navbar.tsx
+++ blogpessoal/src/components/navbar/Navbar.tsx
@@ -20,11 +20,11 @@ function Navbar() {
             <div className='w-full bg-indigo-900 text-white
                 flex justify-center py-4'>
 
-                <div className="container flex justify-between text-lg">
+                <div className="container flex justify-between text-lg px-8">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
-                        Postagens
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
                         Perfil
